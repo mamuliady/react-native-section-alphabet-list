ecba199c802efe27d7f9ad3a11a7390f12aee5bd
Michael Adam <mamuliady@gmail.com>
2021-04-30 20:31:39 +0700

    compile dist

---
+++ dist/components/ListLetterIndex/types.d.ts
@@ -0,0 +1,8 @@
+import { AlphabetListProps, ISectionData } from "../AlphabetList/types";
+export interface ListLetterIndexProps {
+    onPressLetter: (sectionIndex: number) => void;
+    sectionData: ISectionData[];
+    indexLetterStyle?: AlphabetListProps["indexLetterStyle"];
+    indexLetterContainerStyle?: AlphabetListProps["indexLetterContainerStyle"];
+    renderCustomIndexLetter?: AlphabetListProps["renderCustomIndexLetter"];
+}
